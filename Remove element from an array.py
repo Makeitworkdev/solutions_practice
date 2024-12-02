23f5192636ba4cc2eeafc61bd9563f32a3a53d8f
Makeitworkdev <janneyjasmine01@gmail.com>
2024-12-02 16:46:30 +0300

    Update Remove element from an array.py

--- Remove element from an array.py
+++ Remove element from an array.py
@@ -4,7 +4,10 @@ nums = arr.array("i", [35, 65, 72, 19, 35, 88, 89, 52, 22, 43])
 
 val = 35
 
-filtered_arr= arr.array("i", [x for x in nums if x!=val])
+filtered_arr= arr.array("i", [x for x in nums if x!=val])  #remove all instances of val from nums
 
 print(filtered_arr)
-print(len(filtered_arr))
+
+k=len(filtered_arr))
+
+print("The length of the filtered array is: ",k)
